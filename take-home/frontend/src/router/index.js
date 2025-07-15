c2b93b47d8345364db5307ff5adb25ba5575c7b5
Isabella <isabellasdesouza@outlook.com>
2025-07-14 21:08:47 -0300

    feat: implementação views e scripts JavaScript

--- take-home/frontend/src/router/index.js
+++ take-home/frontend/src/router/index.js
@@ -1,30 +1,15 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Router from "vue-router";
+Vue.use(Router);
 
-Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-];
-
-const router = new VueRouter({
+export default new Router({
   mode: "history",
-  base: process.env.BASE_URL,
-  routes,
+  routes: [
+    { path: "/", redirect: "/solicitacao" },
+    { path: "/exames", component: () => import("@/views/Exam/ExameList.vue") },
+    {
+      path: "/solicitacao",
+      component: () => import("@/views/Exam/ExamRequest.vue"),
+    },
+  ],
 });
-
-export default router;
